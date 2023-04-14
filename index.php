1170c1f12571d3cbcb8d9c8aadf121db85714917
diomodiaz <diomedes.diaz2@gmail.com>
2023-04-14 07:07:51 -0500

    Cambio en html

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 $firstNumber = 3;
 $secondNumber = 2;
-$threeNumber = 5;
 
 echo $firstNumber+$secondNumber;
 echo $firstNumber-$secondNumber;
 echo $firstNumber*$secondNumber;
 echo $firstNumber/$secondNumber;
-echo ($firstNumber+$secondNumber)/$threeNumber;
 ?>
